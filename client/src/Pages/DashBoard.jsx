b64b7eaec07c313e9885be23b965d8fc1e677878
AnkitChauhan39 <ankitchauhanbly88@gmail.com>
2024-08-16 01:54:44 +0530

    Added tables in dashboard

--- client/src/Pages/DashBoard.jsx
+++ client/src/Pages/DashBoard.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
-import Tables from '../Components/DashBoardComps/Tables';
+import TablesProducts from '../Components/DashBoardComps/Tables';
+import Prediction from "../Components/DashBoardComps/Tables2"
+import Sply from "../Components/DashBoardComps/Table3"
 
 const DashBoard = () => {
   return (
     <div>
-      <Tables />
+        <div className='p-8'>
+            <div className='text-[28px] font-semibold text-center' >
+                INVENTORY
+            </div>
+            <TablesProducts/>
+        </div>
+        <div className='flex justify-between items-center' >
+            <div className='p-8'>
+                <div className='text-[28px] font-semibold text-center' >
+                    SUPPLY CHAIN
+                </div>
+                <Sply/>
+            </div>
+            <div className='p-8'>
+                <div className='text-[28px] font-semibold text-center' >
+                    PREDICTIONS
+                </div>
+                <Prediction/>
+            </div>
+        </div>
+        <div className='flex justify-center items-center my-10' >
+            <button className=' bg-[#0071CE] text-white px-4 text-[18px] py-2 rounded-xl ' >
+                Show Route
+            </button>
+        </div>
     </div>
   );
 };
